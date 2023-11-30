b8e4ac6884f12b69accbc8c2f8ce74e7f0b57b50
Cezary Marczak <cezary@procyon.ai>
2023-11-30 15:44:27 +0100

    request

--- extensions/guacamole-auth-procyon/src/main/java/org/apache/guacamole/auth/procyon/ProcyonResource.java
+++ extensions/guacamole-auth-procyon/src/main/java/org/apache/guacamole/auth/procyon/ProcyonResource.java
@@ -5,8 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class ProcyonResource {
+
+    private static final Logger logger = Logger.getLogger(ProcyonAuthenticationProvider.class.getName());
 
     @GET
     @Path("/{historyUUID}")
@@ -16,7 +25,41 @@ public class ProcyonResource {
         if (!recordingFile.exists()) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
-        return Response.ok().build();
+
+        String responseMsg = "";
+        try {
+            URL url = new URL("http://guacamole-server:8123/encode");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
+
+            try(DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+                String urlEncodedInputString = "path=/var/lib/guacamole/recordings/" + historyUUID;
+                wr.writeBytes(urlEncodedInputString);
+            }
+
+            StringBuilder response = new StringBuilder();
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String responseLine;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+            }
+
+            logger.info("CZARAS resp from procyon agent: "+response.toString());
+            responseMsg = response.toString();
+
+            connection.disconnect();
+        }
+        catch (Exception e) {
+            logger.log(Level.SEVERE, "CZARAS Error while redirecting to encoder server: " + e.getMessage());
+            // Handle the exception
+            return Response.serverError().type("text/plain").entity("Error while redirecting to encoder server: " + e.getMessage()).build();
+        }
+
+        return Response.ok().type("text/plain").entity("Redirecting to encoder server, msg: "+responseMsg).build();
 
     }
 
